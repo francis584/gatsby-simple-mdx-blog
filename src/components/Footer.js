d7f2ed4b9ef428bba20408fcf8f1cd7cc5ef5373
Francinildo M. Rodrigues <francis584@gmail.com>
2020-07-13 19:52:14 -0300

    add Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,67 @@
+import React from "react"
+
+import { useStaticQuery, graphql } from "gatsby"
+
+// import { RiFacebookBoxLine } from "react-icons/ri"
+
+import {
+  FooterWrapper,
+  FooterSocialWrapper,
+  FooterSocialIcons,
+} from "../elements"
+
+export const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      facebook: file(relativePath: { eq: "facebook.svg" }) {
+        publicURL
+      }
+      linkedin: file(relativePath: { eq: "linkedin.svg" }) {
+        publicURL
+      }
+      instagram: file(relativePath: { eq: "instagram.svg" }) {
+        publicURL
+      }
+      twitter: file(relativePath: { eq: "twitter.svg" }) {
+        publicURL
+      }
+    }
+  `)
+  return (
+    <FooterWrapper>
+      <FooterSocialWrapper>
+        <FooterSocialIcons>
+          <a
+            href="http://facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={data.facebook.publicURL} alt="Facebook logo" />
+          </a>
+          <a
+            href="http://linkedin.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={data.linkedin.publicURL} alt="Linkedin logo" />
+          </a>
+          <a
+            href="http://instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={data.instagram.publicURL} alt="Instagram logo" />
+          </a>
+          <a
+            href="http://twitter.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={data.twitter.publicURL} alt="Twitter logo" />
+          </a>
+        </FooterSocialIcons>
+        <p> 2020 Company. All right reserved.</p>
+      </FooterSocialWrapper>
+    </FooterWrapper>
+  )
+}
